052c44ac4b452e59812aadf17c6ba14e2cbfaa74
hung1720 <coderlazy17@gmail.com>
2024-09-29 22:38:44 +0700

    Update product

--- src/Components/data.js
+++ src/Components/data.js
@@ -4,18 +4,59 @@ export const categories = [
     name: "Hộp khăn giấy",
     products: [
       {
-        id: 1,
+        id: "A07",
         name: "Hộp khăn giấy bánh mì",
         price: "34.000đ",
         image:
           "https://firebasestorage.googleapis.com/v0/b/imagelittleroom.appspot.com/o/A07%2FO1CN010FLgZB1T4GuF2XKXN_!!2213738392328-0-cib.jpg?alt=media&token=007f779f-aaaa-4367-bf74-a9123099f7c1",
       },
       {
-        id: 2,
+        id: "A06",
         name: "Hộp khăn giấy khủng long",
         price: "50.000đ",
         image:
           "https://firebasestorage.googleapis.com/v0/b/imagelittleroom.appspot.com/o/A06%2FO1CN01DSZEIy2CVtJ2HKVSd_!!2210613618480-0-cib.jpg?alt=media&token=dac33dba-9d27-484f-9ff6-031e9e7de002",
+      },
+      {
+        id: "A041",
+        name: "Hộp khăn giấy túi xách",
+        price: "30.000đ",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/imagelittleroom.appspot.com/o/A041%2FO1CN01eDvCcA1UXsDxotcVM_!!2218282382528-0-cib.jpg?alt=media&token=25d92c94-2713-4865-8f60-cb3e5d0c4624",
+      },
+      {
+        id: "A039",
+        name: "Hộp khăn giấy hình nhân vật",
+        price: "43.000đ",
+        image:
+          "https://cbu01.alicdn.com/img/ibank/O1CN01OEtmAi1JnTh2QcG0T_!!2212094761073-0-cib.jpg",
+      },
+      {
+        id: "A045",
+        name: "Hộp khăn giấy hình xe",
+        price: "52.000đ",
+        image:
+          "https://cbu01.alicdn.com/img/ibank/O1CN014t9HAB1MXrIoQRApU_!!2207534701445-0-cib.jpg",
+      },
+      {
+        id: "A046",
+        name: "Hộp khăn giấy đơn giản",
+        price: "62.000đ",
+        image:
+          "https://cbu01.alicdn.com/img/ibank/O1CN01hi7qxt1vrYlXG5vYw_!!4222656226-0-cib.jpg",
+      },
+      {
+        id: "A047",
+        name: "Hộp khăn giấy đôi mắt",
+        price: "42.000đ",
+        image:
+          "https://cbu01.alicdn.com/img/ibank/O1CN01J5q3jT20BwyzSgSE2_!!2216841136812-0-cib.jpg",
+      },
+      {
+        id: "A048",
+        name: "Hộp khăn giấy đơn giản trắng",
+        price: "40.000đ",
+        image: "https://cbu01.alicdn.com/img/ibank/12443187330_53215101.jpg",
       },
     ],
   },
@@ -25,15 +66,17 @@ export const categories = [
     products: [
       {
         id: 3,
-        name: "Móc treo loại A",
-        price: "10.000đ",
-        image: "https://via.placeholder.com/100",
+        name: "Móc treo tường bộ 4 cái",
+        price: "40.000đ",
+        image:
+          "https://cbu01.alicdn.com/img/ibank/O1CN01wtUcKD1RaDGX8u6pZ_!!2211040812127-0-cib.jpg",
       },
       {
         id: 4,
-        name: "Móc treo loại B",
-        price: "15.000đ",
-        image: "https://via.placeholder.com/100",
+        name: "Móc treo tường chữ L.O.V.E",
+        price: "48.000đ",
+        image:
+          "https://cbu01.alicdn.com/img/ibank/O1CN01Wbe8hD1I4JFb5rqHA_!!2217671000839-0-cib.jpg",
       },
     ],
   },
